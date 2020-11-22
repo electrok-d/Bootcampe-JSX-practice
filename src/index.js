678886f5b3a9eb279efd68f7944ce173b31cb2a7
Jed Black <62964873+electrok-d@users.noreply.github.com>
2020-11-22 13:43:44 -0800

    Created ReactDOM render statement that included an unordered list with three elements

--- src/index.js
+++ src/index.js
@@ -2,3 +2,24 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Three reasons to complete Bootcamp</h1>
+    <p>
+      <ul>
+        <li>Make more money</li>
+        <li>Exercise the ability to learn something new</li>
+        <li>Fulfill the desire for a career change.</li>
+      </ul>
+    </p>
+  </div>,
+  document.getElementById("root")
+);
